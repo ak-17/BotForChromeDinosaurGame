12f746b89e84e380d9387a186baf1d0e8b5ce36c
T AKSHAYA KUMAR <takshaya.kumar.civ13@iitbhu.ac.in>
2017-11-08 11:26:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,16 @@ from numpy import *
 
 class Coordinates():
 	replayBtn = (360,230)
+	#coordinates of Replay button
 	dinohead = (125,240)
+	#coordinates of dinosaur head
 
 def restartGame():
 	pyautogui.click(Coordinates.replayBtn)
+	#function to click the replay button
 
 def pressSpace():
+	#implemetation for jump
 	pyautogui.keyDown('space')
 	time.sleep(0.05)
 	print("Jump")
@@ -20,24 +24,27 @@ def pressSpace():
 def imageGrab():
 	#box = (160,425,185,452)
 	box = (Coordinates.dinohead[0]+30,Coordinates.dinohead[1]-13,Coordinates.dinohead[0]+55,Coordinates.dinohead[1]+25)
+	#define a box infront of dinosaur which will be our area of concern 
+	#if we find something in this area we will assume its an obstacle
 	image = ImageGrab.grab(box)
 	grayImage = ImageOps.grayscale(image)
+	#to analyse image in Greyscale
 	a = array(grayImage.getcolors())
+	#convert the image into a numpy array and return the value
 	return a.sum()
 
-restartGame() 
-while(True):
-	if(imageGrab()!=1197):
-		pressSpace()
-		time.sleep(0.05)     
-#def main():
-#	restartGame()
-#	while (True):
-#		if(imageGrab()!=922):
-#			pressSpace()
-#			time.sleep(0.05)
+#if theres nothing present in the area of intrest imageGrab() will give some value,
+# if theres an obstacle in the area of intrest it will give some other value
+#here the value for an empty area of intrest(no obstacle) is 1197
+#we will jump whenever the value is not 1197
+def main():
+	restartGame() 
+	while(True):
+		if(imageGrab()!=1197):
+			pressSpace()
+			time.sleep(0.05)     
 
 
-#main()
+main()
 
 #http://apps.thecodepost.org/trex/trex.html
